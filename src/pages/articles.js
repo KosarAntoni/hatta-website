1dab0c52d829fc8ae974a254bf3d9ed0b2416971
antonikosar <kosarantoni@gmail.com>
2021-04-04 21:27:24 +0200

    Environment configurations; Add components; Add pages;

---
+++ src/pages/articles.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const ArticlesPage = () => (
+    <>
+        <h1>ArticlesPage</h1>
+        <p>Welcome to your new Gatsby site.</p>
+    </>
+)
+
+export default ArticlesPage
---
+++ src/pages/gallery.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const GalleryPage = () => (
+    <>
+        <h1>GalleryPage</h1>
+        <p>Welcome to your new Gatsby site.</p>
+    </>
+)
+
+export default GalleryPage
+
