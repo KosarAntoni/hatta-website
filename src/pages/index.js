6d5b356d32ffa294edd7352a4b6b6f956d7c40d2
antonikosar <kosarantoni@gmail.com>
2021-04-05 16:46:05 +0200

    Add image to hero page;

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,60 @@
-import React from "react";
+import React from "react"
+import { graphql } from "gatsby"
+import Button from "../components/Button/Button"
+import styled from "styled-components"
+import { GatsbyImage } from "gatsby-plugin-image"
 
-const IndexPage = () => (
+const ContentWrapper = styled.div`
+  width: 65%;
+  height: calc(100vh - 80px);
+  text-align: right;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
+
+  h1 {
+    font-size: 85px;
+    width: 70%;
+    line-height: 0.9;
+    margin: 0;
+  }
+
+  p {
+    margin: 20px 0 40px;
+    width: 35%;
+  }
+`
+
+const ImageWrapper = styled(GatsbyImage)`
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 35%;
+  height: 100vh;
+  object-fit: cover;
+`
+
+const IndexPage = ({ data }) => (
   <>
-    <h1>123 Hello Hatta</h1>
+    {console.log(data)}
+    <ContentWrapper>
+      <h1>Your new space</h1>
+      <p>While artists work from real to the abstract, architects must work from the abstract to the
+        real.</p>
+      <Button>estimate project</Button>
+    </ContentWrapper>
+    <ImageWrapper image={data.file.childImageSharp.gatsbyImageData} alt=""/>
   </>
-);
+)
 
-export default IndexPage;
+export const query = graphql`
+  {
+    file(name: {eq: "hero"}) {
+      childImageSharp {
+        gatsbyImageData(layout: FULL_WIDTH, placeholder: TRACED_SVG)
+      }
+    }
+  }
+`
+export default IndexPage
